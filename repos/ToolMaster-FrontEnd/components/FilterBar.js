3e6e16bf8cf54a773be53477f5b695522aabd4da
Andres Miranda <andres.miranda.dev@gmail.com>
2023-04-14 11:54:04 +0900

    fixed the filter bar and made it open as modal when anabling the filter, also added a form reset button and app picker components and form picker component

--- repos/ToolMaster-FrontEnd/components/FilterBar.js
+++ repos/ToolMaster-FrontEnd/components/FilterBar.js
@@ -3,6 +3,8 @@ import {
   Animated,
   TouchableWithoutFeedback,
   View,
+  Modal,
+  Button,
 } from 'react-native';
 import React, { useState } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -12,6 +14,19 @@ import AppFormField from './forms/AppFormField';
 import AppForm from './forms/AppForm';
 import AppText from './AppText';
 import SubmitButton from './SubmitButton';
+import Screen from './Screen';
+import AppPicker from './AppPicker';
+import AppFormPicker from './forms/AppFormPicker';
+import AppPickerItem from './AppPickerItem';
+import AppButton from './AppButton';
+import FormResetButton from './forms/FormResetButton';
+
+const projects = [
+  { name: 'spiralen', projectNumber: 12333, id: 1 },
+  { name: 'drakenberg', projectNumber: 11133, id: 2 },
+  { name: 'tuben', projectNumber: 67633, id: 3 },
+  { name: 'varberg', projectNumber: 44543, id: 4 },
+];
 
 export default function FilterBar({ setData }) {
   const [showFilter, setShowFilter] = useState(false);
@@ -23,60 +38,64 @@ export default function FilterBar({ setData }) {
     setShowFilter(!showFilter);
   };
   return (
-    <Animated.View
-      style={showFilter ? styles.container : styles.hiddenFilterBar}
-    >
-      <View>
-        <View style={styles.head}>
+    <View style={showFilter ? styles.container : styles.hiddenFilterBar}>
+      <View style={styles.head}>
+        <MaterialCommunityIcons
+          name="filter-outline"
+          color={colors.light}
+          size={30}
+        />
+        <AppText style={styles.headText}>Filter</AppText>
+        <TouchableWithoutFeedback onPress={showFilterBar}>
           <MaterialCommunityIcons
-            name="filter-outline"
-            color={colors.light}
+            name="chevron-down"
             size={30}
+            color={colors.medium}
           />
-          <AppText style={styles.headText}>Filter</AppText>
-          <TouchableWithoutFeedback onPress={showFilterBar}>
-            <MaterialCommunityIcons
-              name="chevron-down"
-              size={30}
-              color={colors.medium}
-            />
-          </TouchableWithoutFeedback>
-        </View>
-        <AppForm
-          initialValues={{
-            name: '',
-            serieNumber: '',
-            project: '',
-            available: '',
-            toolGroup: '',
-          }}
-          onSubmit={handleSubmit}
-        >
-          <AppFormField icon="text-search" name="name" placeholder="Name" />
-          <AppFormField
-            icon="text-search"
-            name="serieNumber"
-            placeholder="Serie Nr"
-          />
-          <AppFormField
-            icon="text-search"
-            name="project"
-            placeholder="Projekt"
-          />
-          <AppFormField
-            icon="text-search"
-            name="available"
-            placeholder="Status"
-          />
-          <AppFormField
-            icon="text-search"
-            name="toolGroup"
-            placeholder="Group"
-          />
-          <SubmitButton title="search" />
-        </AppForm>
+        </TouchableWithoutFeedback>
       </View>
-    </Animated.View>
+      <Modal visible={showFilter} animationType="fade">
+        <Screen>
+          <Button title="close" onPress={() => setShowFilter(false)} />
+          <AppForm
+            initialValues={{
+              name: '',
+              serieNumber: '',
+              project: '',
+              available: '',
+              toolGroup: '',
+            }}
+            onSubmit={handleSubmit}
+          >
+            <AppFormField icon="text-search" name="name" placeholder="Name" />
+            <AppFormField
+              icon="text-search"
+              name="serieNumber"
+              placeholder="Serie Nr"
+            />
+            <AppFormPicker
+              items={projects}
+              name="project"
+              placeholder="Projekt"
+              width="50%"
+              PickerItemComponent={AppPickerItem}
+            />
+            <AppFormField
+              icon="text-search"
+              name="available"
+              placeholder="Status"
+            />
+            <AppFormField
+              icon="text-search"
+              name="toolGroup"
+              placeholder="Group"
+            />
+            <SubmitButton title="search" />
+            <FormResetButton title="reset" color="secondary" />
+          </AppForm>
+        </Screen>
+      </Modal>
+    </View>
   );
 }
 
---
+++ repos/ToolMaster-FrontEnd/components/forms/AppFormPicker.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppPicker from '../AppPicker';
+import { useFormikContext } from 'formik';
+import ErrorMessage from '../ErrorMessage';
+
+export default function AppFormPicker({
+  items,
+  name,
+  numberOfColumns,
+  placeholder,
+  PickerItemComponent,
+  width,
+}) {
+  const { errors, setFieldValue, touched, values } = useFormikContext();
+
+  return (
+    <>
+      <AppPicker
+        items={items}
+        nummberOfColumns={numberOfColumns}
+        onSelectItem={(item) => setFieldValue(name, item)}
+        placeholder={placeholder}
+        PickerItemComponent={PickerItemComponent}
+        selectedItem={values[name]}
+        width={width}
+      />
+      <ErrorMessage error={errors[name]} visible={touched} />
+    </>
+  );
+}
